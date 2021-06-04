44bedda7554f7a58fc38683eaced89f77f857f07
wsruser1 <alekseev_lnt@mail.ru>
2021-06-04 16:01:24 +0300

    Initial Commit

---
+++ zadanie1/zadanie1App.swift
@@ -0,0 +1,17 @@
+//
+//  zadanie1App.swift
+//  zadanie1
+//
+//  Created by user on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct zadanie1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
