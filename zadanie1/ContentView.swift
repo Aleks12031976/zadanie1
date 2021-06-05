d217da2f87d28a92a029ad9b3139045e3f2bcf25
wsruser1 <alekseev_lnt@mail.ru>
2021-06-05 09:26:48 +0300

    Add Sign UP

--- zadanie1/ContentView.swift
+++ zadanie1/ContentView.swift
@@ -6,14 +6,45 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State var mail = ""
+    @State var pas = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack( content: {
+        
+        
+                Image("planeta")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding()
+                Text("EMAIL")
+                    .frame(width: 300, height: 0, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+            TextField("name@email.com",text: $mail)
+                    .textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal,50)
+                
+                Text("PASSWORD")
+                    .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                TextField("**********", text: $pas)
+                    .textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal,5)
+                Button(action: {}, label: {
+                    Text("SIGN APP")
+                        .background(Color(.white))
+                        .frame(width: 300, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color(.blue))
+                        .cornerRadius(10)
+                })
+                
+            })
+            
+        }
     }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
